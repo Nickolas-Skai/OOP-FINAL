d9e6db85cdb6752bde792c70d7c5d3b5eaa0baf0
D'Alesseo Requena <89269942+Nickolas-Skai@users.noreply.github.com>
2024-05-12 18:29:04 -0600

    added some function defintions
    help me :)

--- adminusers.cpp
+++ adminusers.cpp
@@ -1,5 +1,5 @@
 #include "adminusers.h"
-
+#include "database.h"
 Adminusers::Adminusers() {
 
 }
@@ -37,21 +37,113 @@ QString Adminusers::Add_User(QString fname, QString lname, QString username, QSt
 
 
 QString Adminusers::Edit_User(QString fname, QString lname, QString username, QString paswd, QString date_ofbirth, QString phon, QString email, bool admin) {
+    //did by seo idk what im doing tho
+    if(!getAdmin()) {
+        return "Error: only admins can edit user.";
+    }
+
+    try
+    {
+        if(db.editUser(fname, lname, username, paswd, date_ofbirth, phon, email, admin)){
+            throw ("User Edited Successfully!");
+        }
+        else{
+            throw ("An error occured while saving to the database!");
+        }
+    }
+    catch(const char * message)
+    {
+        return message;
+    }
 
 }
 
 QString Adminusers::Add_Room(int roomnumber, QString Roomtype, int capacity, double pricepernight, bool availability) {
 
+    if(!getAdmin()) {
+        return "Error: only admins can add room.";
+    }
+
+    try
+    {
+        if(db.addRoom(roomnumber, Roomtype, capacity, pricepernight, availability)){
+            throw ("Room Added Successfully!");
+        }
+        else{
+            throw ("An error occured while saving to the database!");
+        }
+    }
+    catch(const char * message)
+    {
+        return message;
+    }
+
 }
 
 QString Adminusers::Edit_Room(int roomnumber, QString roomtype, int capacity, double pricepernight, bool availability) {
+
+    if(!getAdmin()) {
+        return "Error: only admins can edit room.";
+    }
+
+    try
+    {
+        if(db.editRoom(roomnumber, roomtype, capacity, pricepernight, availability)){
+            throw ("Room Edited Successfully!");
+        }
+        else{
+            throw ("An error occured while saving to the database!");
+        }
+    }
+    catch(const char * message)
+    {
+        return message;
+    }
 
 }
 
 QString Adminusers::Add_Amenitie(QString name, QString description, double price) {
 
+    if(!getAdmin()) {
+        return "Error: only admins can add amenitie.";
+    }
+
+    try
+    {
+        if(db.addAmenity(name, description, price)){
+            throw ("Amenitie Added Successfully!");
+        }
+        else{
+            throw ("An error occured while saving to the database!");
+        }
+    }
+    catch(const char * message)
+    {
+        return message;
+    }
+
+
 }
 
 QString Adminusers::Edit_Amenities(QString name, QString description, double price) {
 
+    if (!getAdmin()) {
+        return "Error: only admins can edit amenities.";
+    }
+    try
+    {
+
+        if(db.editAmenitity(name, description, price)){
+            throw ("Amenities Edited Successfully!");
+        }
+        else{
+            throw ("An error occured while saving to the database!");
+        }
+    }
+    catch(const char * message)
+    {
+        return message;
+    }
+
 }
+
